cdace3542b13163607402d9f6320134fcd69c0c4
Lyndon Coghill <lcoghill@fieldmuseum.org>
2013-11-21 11:03:40 -0600

    Added fucntionality to build a taxonomy graph for the OTT taxonomy as well as the NCBI.

--- scripts/make-taxonomy-graph.py
+++ scripts/make-taxonomy-graph.py
@@ -1,7 +1,44 @@
 from ivy import treegraph as tg
 
-# this may take a few minutes
-g = tg.create_ncbi_taxonomy_graph(basepath='ncbi')
+choice = raw_input("Build a taxonomy graph for (1)NCBI, (2)OTT or (3)BOTH:")
 
-# save the graph to a GraphML file
-g.save('ncbi/ncbi.xml.gz')
+if choice == "1":
+
+    ## Builds a graph with the NCBI taxonomy
+    print "Loading NCBI taxonomy into graph..."
+    # save the graph to a GraphML file
+    g = tg.create_ncbi_taxonomy_graph(basepath='taxonomy/ncbi')
+    # save the graph to a GraphML file
+    g.save('taxonomy/ncbi/ncbi.xml.gz')
+    print "NCBI taxonomy Graph Complete."
+
+
+elif choice == "2":
+
+    ## Builds a graph with the OTT taxonomy
+    print "Loading OTT taxonomy into graph..."
+    g = tg.create_opentree_taxonomy_graph(basepath='taxonomy/ott2.2')
+    # save the graph to a GraphML file
+    g.save('taxonomy/ott2.2/ott2.2.xml.gz')
+    print "OTT taxonomy Graph Complete."
+
+elif choice == "3":
+
+    ## Builds a graph with the NCBI taxonomy
+    print "Loading NCBI taxonomy into graph..."
+    # save the graph to a GraphML file
+    g = tg.create_ncbi_taxonomy_graph(basepath='taxonomy/ncbi')
+    g.save('taxonomy//ncbi//ncbi.xml.gz')
+    print "NCBI taxonomy Graph Complete."
+
+    ## Builds a graph with the NCBI taxonomy
+    print "Loading OTT taxonomy into graph..."
+    g = tg.create_opentree_taxonomy_graph(basepath='taxonomy/ott2.2')
+    # save the graph to a GraphML file
+    g.save('taxonomy/ott2.2/ott2.2.xml.gz')
+    print "OTT taxonomy Graph Complete."
+
+else:
+
+    # Catch all for an invalid menu choice
+    print "Not a valid choice. Please try again"
